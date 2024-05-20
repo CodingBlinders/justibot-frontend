7dd8c628d5b27de3483ec82ca10ce3b87fd4db40
yashed <--add>
2024-05-20 19:10:29 +0530

    change chat page

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useRef, useEffect } from "react";
 import ChatNacBar from "../components/chatNavBar";
+import Image from "next/image";
 
 const Chat = () => {
   const textareaRef = useRef<HTMLTextAreaElement>(null);
@@ -21,12 +22,22 @@ const Chat = () => {
 
   return (
     <div className="grid grid-cols-12 overflow-hidden h-screen w-screen bg-[#131211]">
-      <div className="sm:col-span-0 lg:col-span-2 md:col-span-3 p-4 justify-center items-center">
+      <div className="sm:col-span-0 lg:col-span-3 xl:col-span-2 md:col-span-3 p-4 justify-center items-center">
         <ChatNacBar />
       </div>
-      <div className="col-span-12 lg:col-span-10 md:col-span-9 p-4 justify-center items-center w-full">
+      <div className="col-span-12 lg:col-span-9 xl:col-span-10 md:col-span-9 p-4 justify-center items-center w-full">
         <div className="m-1 rounded-[50px] bg-[#1F1D1C] border-2 border-white h-full">
-          <form className="relative h-full w-full">
+          <form className="relative h-full w-full flex justify-center">
+            <div className="w-52 h-auto m-20">
+              <Image
+                src="/images/justibot-logo-1.png"
+                alt="justibot-logo-1"
+                width={400}
+                height={400}
+                layout="responsive"
+                objectFit="cover"
+              />
+            </div>
             <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 mb-4 flex items-center justify-center w-full">
               <textarea
                 id="message"
@@ -37,7 +48,7 @@ const Chat = () => {
                 placeholder="Message JustiBot.."
                 onInput={handleInput}
               />
-              <button className="flex items-center py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline group">
+              <button className="flex items-center py-2 rounded-lg focus:outline-none focus:shadow-outline group">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="29"
