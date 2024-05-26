394fc5483419288473fee65f4f21214e75097027
isuru-liyanage <isuruliyanageofficial@gmail.com>
2024-05-26 12:08:24 +0530

    config next ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,17 @@
 import type { Config } from "tailwindcss";
-
+import {nextui} from "@nextui-org/react";
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      colors: {
+        customBrown: '#A36A32',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -16,8 +20,10 @@ const config: Config = {
       scrollbar: ['rounded'],
     },
   },
+
+  darkMode: "class",
   plugins: [
-   
+      nextui(),
     require('tailwind-scrollbar')({ nocompatible: true }),
   ],
 };
